e9d9c85d8698b4de1342fe33b32d41b126358bbd
Arkadiusz Rasz <arkadiusz.rasz@gmail.com>
2017-12-08 21:53:33 +0100

    ImGui

--- main.cpp
+++ main.cpp
@@ -1,24 +1,46 @@
-#include <SFML/Graphics.hpp>
-#include <cmath>
+#include "imgui/imgui.h"
+#include "imgui/imgui-SFML.h"
 
-int main(){
-    sf::RenderWindow app(sf::VideoMode(320, 240), "Example");
-    sf::Clock stoper;
-    while(app.isOpen()){
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
         sf::Event event;
-        while(app.pollEvent(event)){
-            if(event.type == sf::Event::Closed)
-                 app.close();
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
         }
-        app.clear();
 
-        sf::CircleShape shape(std::sin( stoper.getElapsedTime().asSeconds()) * app.getSize().y / 8 + app.getSize().y / 4);
-        shape.setOrigin(sf::Vector2f( shape.getRadius(), shape.getRadius()));
-        shape.setPosition(app.getSize().x / 2.0f, app.getSize().y / 2.0f);
-        shape.setFillColor(sf::Color::Yellow);
-        app.draw(shape);
+        ImGui::SFML::Update(window, deltaClock.restart());
 
-        app.display();
+        ImGui::ShowTestWindow();
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
     }
+
+    ImGui::SFML::Shutdown();
+
     return 0;
 }
