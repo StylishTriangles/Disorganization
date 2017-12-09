90719abb50d92b8db217a7f70a78716035d190a3
Arkadiusz Rasz <arkadiusz.rasz@gmail.com>
2017-12-09 18:30:41 +0100

    Kot psotnik

--- game.cpp
+++ game.cpp
@@ -122,10 +122,10 @@ void Game::createObjects(){
     items["pot"] = new ItemPot(anims["pot"], 1.0f);
     items["pot"]->move(600, 100);
 
-    //items["pot2"] = new ItemPot(anims["pot"], -3.f);
-    //items["pot2"]->move(100, 100);
-    //items["pot3"] = new ItemPot(anims["pot"]);
-    //items["pot3"]->move(200, 200);
+    items["pot2"] = new ItemPot(anims["pot"], -3.f);
+    items["pot2"]->move(100, 100);
+    items["pot3"] = new ItemPot(anims["pot"]);
+    items["pot3"]->move(200, 200);
 
     pranks.push_back(new PrankBookThrow(this));
 
