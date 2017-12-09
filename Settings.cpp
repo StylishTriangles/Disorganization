40f689b8e2493410cbc452f38f2e4c9f887e0274
Arkadiusz Rasz <arkadiusz.rasz@gmail.com>
2017-12-09 21:49:30 +0100

    settings

---
+++ Settings.cpp
@@ -0,0 +1,4 @@
+#include "Settings.hpp"
+
+sf::Vector2f Settings::windowSize = sf::Vector2f(1280, 720);
+int Settings::floorLevel = 400;
---
+++ Settings.hpp
@@ -0,0 +1,13 @@
+#ifndef SETTINGS_HPP
+#define SETTINGS_HPP
+
+#include <SFML/System.hpp>
+
+
+class Settings {
+public :
+	static sf::Vector2f windowSize;
+	static int floorLevel;
+};
+
+#endif //SETTINGS_HPP
